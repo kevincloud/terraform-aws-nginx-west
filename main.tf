b9a145396a9bc68d3149ead0c6580cea77f6b293
Kevin Cochran <kcochran@hashicorp.com>
2019-02-11 11:49:46 -0500

    update region

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
-    region = "us-east-1"
+    region = "us-west-2"
 }
 
 resource "aws_instance" "nginx_instance" {
