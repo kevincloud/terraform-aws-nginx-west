398daaeded4caeb6872214ef49e20ad037eb16df
Kevin Cochran <kcochran@hashicorp.com>
2019-02-11 11:58:05 -0500

    pass provider creds

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
+variable "aws_access_key" {
+    description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+    description = "AWS secret key"
+}
+
 variable "server_name" {
   description = "Name of the server"
   default = "nginx-cc"
